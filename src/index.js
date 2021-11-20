86b0513694f6e30a487cc64d2d427624edea91ff
randymbemba <79250549+randymbemba@users.noreply.github.com>
2021-11-20 18:06:57 +0100

    parametrage de son

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import {hello} from "./hello";
 import "./styles/styles.css";
 
-hello();
-hello();
-hello();
-hello();
+let btn;
 
-console.log("hello webpack");
+function init(){
+    btn = document.querySelector('.boutons');
+
+    btn.addEventListener('click',() =>{
+        sound();
+    });
+
+    function sound(){
+        var context = new AudioContext();
+        var o = context.createOscillator();
+        var  g = context.createGain();
+        g.gain.value = 0.1;
+        o.connect(g)
+        o.connect(context.destination);
+        o.start(0);
+
+        g.gain.exponentialRampToValueAtTime(
+            0.00001, context.currentTime + 2
+        )
+    };
+    sound();
+}
+
+window.onload = init;
