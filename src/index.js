b9f5319d1bd81d026e9803f40b40faad4855b29c
randymbemba <79250549+randymbemba@users.noreply.github.com>
2021-11-21 04:52:52 +0100

    modif css correction des boucles affichage des boutons

--- src/index.js
+++ src/index.js
@@ -8,23 +8,82 @@ let btn = {
     jaune: null
 };
 
+let intervalFunction;
+
+let couleur = ['rouge','bleu','vert','jaune'];
+let nombre = 3
+let game = [];
+
+const colorQueue = [];
+
+
 function init(){
-        let array1 = document.querySelectorAll(".boutons");
-        array1.forEach(element =>  {
-            element.addEventListener("click",() =>{
-                sound();
-            })
-        });
-        
-    function sound(){
-        const synth = new Tone.Synth().toDestination();
-        const now = Tone.now()
-        synth.triggerAttackRelease("C4", "8n", now)
-        synth.triggerAttackRelease("E4", "8n", now + 0.5)
-        synth.triggerAttackRelease("G4", "8n", now + 1)
-        synth.triggerAttackRelease("F4", "8n", now + 1.5)
+    let array1 = document.querySelectorAll(".boutons");
+    array1.forEach(element =>  {
+        element.addEventListener("click",() =>{
+            if (element.id === "jouer"){
+                for (let i = 0; i<nombre;i++){
+                    let pos = Math.round(Math.random() * (couleur.length - 1))
+                    game.push(couleur[pos])
+                }
+                let position = 0
+                intervalFunction = setInterval(
+                    () =>{
+                        const selectPosition = game[position]
+
+                    },1000
+                )
+            }else if (element.id === "rouge"){
+                const synth = new Tone.Synth().toDestination();
+                const now = Tone.now()
+                synth.triggerAttackRelease("C4", "8n", now)
+            }else if (element.id === "bleu"){
+                const synth = new Tone.Synth().toDestination();
+                const now = Tone.now()
+                synth.triggerAttackRelease("E4", "8n", now)
+
+            }else if (element.id === "jaune"){
+                const synth = new Tone.Synth().toDestination();
+                const now = Tone.now()
+                synth.triggerAttackRelease("G4", "8n", now)
+
+            }else if (element.id === "vert"){
+                const synth = new Tone.Synth().toDestination();
+                const now = Tone.now()
+                synth.triggerAttackRelease("F4", "8n", now )
+            }
+        })
+
+    });
+
+    const addColor = () => {
+        colorQueue.push(pos)
     }
-    sound();
 }
+
+function debut () {
+    let arra = document.querySelectorAll(".boutons");
+    arra.forEach(element =>  {
+        element.addEventListener("click",() =>{
+            if (element.id === "jouer"){
+                for (let i = 0; i < nombre; i++) {
+                    const pos = Math.round(Math.random() * (couleur.length - 1))
+                    game.push(couleur[pos])
+                }
+                console.log(game);
+                /* let position = 0
+                 intervalFunction = setInterval(
+                     () => {
+                         const selectPosition = game[position]
+
+                     }, 1000
+                 )*/
+            }else {
+
+            }
+        })
+ })
+}
+debut();
 
 window.onload = init;
